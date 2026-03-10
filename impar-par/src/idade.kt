bd3ea6e87bf3e67f8ca736fde2ea74fbb3351692
Kauê <rodiguesvitorkaue@gmail.com>
2026-03-10 20:04:56 -0300

    Primerias aulas sobre Kotlin

---
+++ impar-par/src/idade.kt
@@ -0,0 +1,12 @@
+fun main(){
+    val idade = 10
+
+    if (idade < 12){
+        println("Criança")
+    }else if (idade > 12 || idade < 18){
+        println("Adolecente")
+    }else{
+        println("O numero é zero")
+    }
+
+}
---
+++ impar-par/src/positivo_negativo.kt
@@ -0,0 +1,12 @@
+fun main(){
+    val numero = 10
+
+    if (numero > 0){
+        println("O numero é positivo")
+    }else if (numero < 0){
+        println("O numero é negativo")
+    }else{
+        println("O numero é zero")
+    }
+
+}
